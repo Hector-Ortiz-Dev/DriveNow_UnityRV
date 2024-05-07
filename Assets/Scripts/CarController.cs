2bda0b60c54410e8fb17a62a3403af8f2c038399
LuisEsparza-bg <luis.esparzalz@uanl.edu.mx>
2024-05-07 03:27:46 -0600

    Colisiones de las Zonas de Niveles, Camaras para espejos y Nivel 2. --charlyx78
    Faltante:
    
    1- Todavía falta que los espejos esten bien puestos
    2- Sonidos
    3- Collisiones con otros objetos
    4 - Mover la cámara pero eso ya sería mejor verlo con lo del Oculus.
    5  - Mejorar lo de las colisiones del carro con las zonas de estacionamiento
    6 - GUI o UI

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -9,11 +9,19 @@ public class CarController : MonoBehaviour
     private float verticalInput;
     private float steerAngle;
     private bool isBreaking;
+    
 
     public WheelCollider frontLeftWheelCollider;
     public WheelCollider frontRightWheelCollider;
     public WheelCollider rearLeftWheelCollider;
     public WheelCollider rearRightWheelCollider;
+    public BoxCollider volumeCar;
+    public BoxCollider volumeFirstLevel;
+    public BoxCollider volumeSecondLevel;
+    public BoxCollider volumeThirdLevel;
+    public bool volumen1 = false;
+    public bool volumen2 = false;
+    public bool volumen3 = false;
     public Transform frontLeftWheelTransform;
     public Transform frontRightWheelTransform;
     public Transform rearLeftWheelTransform;
@@ -30,7 +38,109 @@ public class CarController : MonoBehaviour
         HandleMotor();
         HandleSteering();
         UpdateWheels();
+
+
+        if (volumen1 == true)
+        {
+            float porcentajeEnVolume = CalcularPorcentajeEnVolume(volumeCar, volumeFirstLevel);
+            Debug.Log("Porcentaje en el volumen del nivel 1: " + porcentajeEnVolume + "%");
+        }
+
+
+        if (volumen2 == true)
+        {
+            float porcentajeEnVolume = CalcularPorcentajeEnVolume(volumeCar, volumeSecondLevel);
+            Debug.Log("Porcentaje en el volumen del nivel 2: " + porcentajeEnVolume + "%");
+        }
+
+        if (volumen3 == true)
+        {
+            float porcentajeEnVolume = CalcularPorcentajeEnVolume(volumeCar, volumeThirdLevel);
+            Debug.Log("Porcentaje en el volumen del nivel 3: " + porcentajeEnVolume + "%");
+        }
+
+
+
     }
+
+
+    private float CalcularPorcentajeEnVolume(BoxCollider carroCollider, BoxCollider volumenCollider)
+    {
+        // Calculamos el volumen del área de intersección entre los colliders
+        float volumenInterseccion = VolumenInterseccion(carroCollider, volumenCollider);
+
+        // Calculamos el volumen total del box collider del carro
+        float volumenCarro = carroCollider.size.x * carroCollider.size.y * carroCollider.size.z;
+
+        // Calculamos el porcentaje del carro dentro del volumen
+        float porcentajeEnVolume = (volumenInterseccion / volumenCarro) * 100f;
+
+        return porcentajeEnVolume;
+    }
+
+    private float VolumenInterseccion(BoxCollider collider1, BoxCollider collider2)
+    {
+        // Obtenemos la intersección de los bounds de los colliders
+        Bounds intersectionBounds = InterseccionBounds(collider1.bounds, collider2.bounds);
+
+        // Calculamos el volumen del área de intersección
+        float volumenInterseccion = intersectionBounds.size.x * intersectionBounds.size.y * intersectionBounds.size.z;
+
+        return volumenInterseccion;
+    }
+
+    private Bounds InterseccionBounds(Bounds bounds1, Bounds bounds2)
+    {
+        // Calculamos los límites de la intersección
+        float minX = Mathf.Max(bounds1.min.x, bounds2.min.x);
+        float minY = Mathf.Max(bounds1.min.y, bounds2.min.y);
+        float minZ = Mathf.Max(bounds1.min.z, bounds2.min.z);
+        float maxX = Mathf.Min(bounds1.max.x, bounds2.max.x);
+        float maxY = Mathf.Min(bounds1.max.y, bounds2.max.y);
+        float maxZ = Mathf.Min(bounds1.max.z, bounds2.max.z);
+
+        // Creamos y devolvemos los bounds de la intersección
+        return new Bounds(new Vector3((minX + maxX) / 2, (minY + maxY) / 2, (minZ + maxZ) / 2),
+                          new Vector3(maxX - minX, maxY - minY, maxZ - minZ));
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other == volumeFirstLevel)
+        {
+            volumen1 = true;
+        }
+
+        if (other == volumeSecondLevel)
+        {
+            volumen2 = true;
+        }
+
+        if (other == volumeThirdLevel)
+        {
+            volumen3 = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other == volumeFirstLevel)
+        {
+            volumen1 = false;
+        }
+
+        if (other == volumeSecondLevel)
+        {
+            volumen2 = true;
+        }
+
+        if (other == volumeThirdLevel)
+        {
+            volumen3 = true;
+        }
+
+    }
+
 
     private void GetInput()
     {
