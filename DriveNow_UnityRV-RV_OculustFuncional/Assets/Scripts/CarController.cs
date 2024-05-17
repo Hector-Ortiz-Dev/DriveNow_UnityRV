c2f580eaa01a37025404d7c7500e9e04ef1bf494
karlaMMs <126914568+karlaMMs@users.noreply.github.com>
2024-05-17 07:18:17 -0600

    final

---
+++ DriveNow_UnityRV-RV_OculustFuncional/Assets/Scripts/CarController.cs
@@ -0,0 +1,355 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    private float horizontalInput;
+    private float verticalInput;
+    private float steerAngle;
+    private bool isBreaking;
+    
+
+    public WheelCollider frontLeftWheelCollider;
+    public WheelCollider frontRightWheelCollider;
+    public WheelCollider rearLeftWheelCollider;
+    public WheelCollider rearRightWheelCollider;
+    public BoxCollider volumeCar;
+    public BoxCollider volumeFirstLevel;
+    public BoxCollider volumeSecondLevel;
+    public BoxCollider volumeThirdLevel;
+    public Transform carroTransform;
+    public bool volumen1 = false;
+    public bool volumen2 = false;
+    public bool volumen3 = false;
+    public bool victoria1 = false;
+    public bool victoria2 = false;
+    public bool victoria3 = false;
+    public bool conteoActivado1 = false;
+    public bool conteoActivado2 = false;
+    public bool conteoActivado3 = false;
+    private float tiempoConteo = 0f;
+    private float duracionConteo = 5f;
+    public Transform frontLeftWheelTransform;
+    public Transform frontRightWheelTransform;
+    public Transform rearLeftWheelTransform;
+    public Transform rearRightWheelTransform;
+
+    public float maxSteeringAngle = 30f;
+    public float motorForce = 50f;
+    public float brakeForce = 0f;
+
+
+    private void FixedUpdate()
+    {
+        GetInput();
+        HandleMotor();
+        HandleSteering();
+        UpdateWheels();
+
+
+        if (conteoActivado1)
+        {
+
+            Debug.Log("Contando..." + tiempoConteo);
+
+            tiempoConteo += Time.deltaTime;
+            if (tiempoConteo >= duracionConteo)
+            {
+
+                conteoActivado1 = false;
+                tiempoConteo = 0f;
+                victoria1 = true;
+
+                if (victoria1)
+                {
+                    string MensajeVictoria = Victoria(carroTransform, 1);
+                    Debug.Log("¡Victoria en el nivel 1!");
+                    Debug.Log("¡Calificació:" + MensajeVictoria);
+                    conteoActivado1 = false;
+                    volumeFirstLevel.enabled = false;
+                }
+               
+            }
+        }
+
+
+        if (conteoActivado2)
+        {
+
+            Debug.Log("Contando..." + tiempoConteo);
+
+            tiempoConteo += Time.deltaTime;
+            if (tiempoConteo >= duracionConteo)
+            {
+
+                conteoActivado2 = false;
+                tiempoConteo = 0f;
+                victoria2 = true;
+
+                if (victoria2)
+                {
+
+                    Debug.Log("¡Victoria en el nivel 2!");
+                    conteoActivado2 = false;
+                    volumeSecondLevel.enabled = false;
+                }
+
+            }
+        }
+
+        if (conteoActivado3)
+        {
+
+            Debug.Log("Contando..." + tiempoConteo);
+
+            tiempoConteo += Time.deltaTime;
+            if (tiempoConteo >= duracionConteo)
+            {
+
+                conteoActivado3 = false;
+                tiempoConteo = 0f;
+                victoria3 = true;
+
+                if (victoria3)
+                {
+                    Debug.Log("¡Victoria en el nivel 3!");
+                    conteoActivado1 = false;
+                    volumeThirdLevel.enabled = false;
+                }
+
+            }
+        }
+
+
+        if (volumen1 == true)
+        {
+            float porcentajeEnVolume = CalcularPorcentajeEnVolume(volumeCar, volumeFirstLevel);
+            Debug.Log("Porcentaje en el volumen del nivel 1: " + porcentajeEnVolume + "%");
+
+            if (porcentajeEnVolume >= 105 && porcentajeEnVolume <= 130f)
+            {
+                conteoActivado1 = true;
+            }
+            else
+            {
+                conteoActivado1 = false;
+                tiempoConteo = 0f;
+            }
+        }
+
+
+        if (volumen2 == true)
+        {
+            float porcentajeEnVolume = CalcularPorcentajeEnVolume(volumeCar, volumeSecondLevel);
+            Debug.Log("Porcentaje en el volumen del nivel 2: " + porcentajeEnVolume + "%");
+
+            if (porcentajeEnVolume >= 105 && porcentajeEnVolume <= 130f)
+            {
+                conteoActivado2 = true;
+            }
+            else
+            {
+                conteoActivado2 = false;
+                tiempoConteo = 0f;
+            }
+
+
+        }
+
+        if (volumen3 == true)
+        {
+            float porcentajeEnVolume = CalcularPorcentajeEnVolume(volumeCar, volumeThirdLevel);
+            Debug.Log("Porcentaje en el volumen del nivel 3: " + porcentajeEnVolume + "%");
+
+
+            Vector3 angulos = carroTransform.eulerAngles;
+
+
+            if (porcentajeEnVolume >= 105 && porcentajeEnVolume <= 130f)
+            {
+                conteoActivado3 = true;
+            }
+            else
+            {
+                conteoActivado3 = false;
+                tiempoConteo = 0f;
+            }
+
+
+            //if (((angulos.x >= anguloMinimo && angulos.x <= anguloMaximo) || (angulos.x >= 360 - anguloMaximo && angulos.x <= 360 - anguloMinimo)) &&
+            //     ((angulos.y >= -5f && angulos.y <= 0f) || (angulos.y >= 175f && angulos.y <= 185f) || (angulos.y >= 360 - 5f && angulos.y <= 360f)) &&
+            //    ((angulos.z >= anguloMinimo && angulos.z <= anguloMaximo) || (angulos.z >= 360 - anguloMaximo && angulos.z <= 360 - anguloMinimo)))
+            //{
+            //}
+
+        }
+
+
+
+    }
+
+
+    private float CalcularPorcentajeEnVolume(BoxCollider carroCollider, BoxCollider volumenCollider)
+    {
+        // Calculamos el volumen del área de intersección entre los colliders
+        float volumenInterseccion = VolumenInterseccion(carroCollider, volumenCollider);
+
+        // Calculamos el volumen total del box collider del carro
+        float volumenCarro = carroCollider.size.x * carroCollider.size.y * carroCollider.size.z;
+
+        // Calculamos el porcentaje del carro dentro del volumen
+        float porcentajeEnVolume = (volumenInterseccion / volumenCarro) * 100f;
+
+
+            return porcentajeEnVolume;
+    }
+
+
+  
+
+
+    private float VolumenInterseccion(BoxCollider collider1, BoxCollider collider2)
+    {
+        // Obtenemos la intersección de los bounds de los colliders
+        Bounds intersectionBounds = InterseccionBounds(collider1.bounds, collider2.bounds);
+
+        // Calculamos el volumen del área de intersección
+        float volumenInterseccion = intersectionBounds.size.x * intersectionBounds.size.y * intersectionBounds.size.z;
+
+        return volumenInterseccion;
+    }
+
+    private Bounds InterseccionBounds(Bounds bounds1, Bounds bounds2)
+    {
+        // Calculamos los límites de la intersección
+        float minX = Mathf.Max(bounds1.min.x, bounds2.min.x);
+        float minY = Mathf.Max(bounds1.min.y, bounds2.min.y);
+        float minZ = Mathf.Max(bounds1.min.z, bounds2.min.z);
+        float maxX = Mathf.Min(bounds1.max.x, bounds2.max.x);
+        float maxY = Mathf.Min(bounds1.max.y, bounds2.max.y);
+        float maxZ = Mathf.Min(bounds1.max.z, bounds2.max.z);
+
+        // Creamos y devolvemos los bounds de la intersección
+        return new Bounds(new Vector3((minX + maxX) / 2, (minY + maxY) / 2, (minZ + maxZ) / 2),
+                          new Vector3(maxX - minX, maxY - minY, maxZ - minZ));
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other == volumeFirstLevel)
+        {
+            volumen1 = true;
+        }
+
+        if (other == volumeSecondLevel)
+        {
+            volumen2 = true;
+        }
+
+        if (other == volumeThirdLevel)
+        {
+            volumen3 = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other == volumeFirstLevel)
+        {
+            volumen1 = false;
+        }
+
+        if (other == volumeSecondLevel)
+        {
+            volumen2 = false;
+        }
+
+        if (other == volumeThirdLevel)
+        {
+            volumen3 = false;
+        }
+
+    }
+
+
+    private void GetInput()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        isBreaking = Input.GetKey(KeyCode.Space);
+    }
+
+    private void HandleSteering()
+    {
+        steerAngle = maxSteeringAngle * horizontalInput;
+        frontLeftWheelCollider.steerAngle = steerAngle;
+        frontRightWheelCollider.steerAngle = steerAngle;
+    }
+
+    private void HandleMotor()
+    {
+        frontLeftWheelCollider.motorTorque = verticalInput * motorForce;
+        frontRightWheelCollider.motorTorque = verticalInput * motorForce;
+
+        brakeForce = isBreaking ? 3000f : 0f;
+        frontLeftWheelCollider.brakeTorque = brakeForce;
+        frontRightWheelCollider.brakeTorque = brakeForce;
+        rearLeftWheelCollider.brakeTorque = brakeForce;
+        rearRightWheelCollider.brakeTorque = brakeForce;
+    }
+
+    private void UpdateWheels()
+    {
+        UpdateWheelPos(frontLeftWheelCollider, frontLeftWheelTransform);
+        UpdateWheelPos(frontRightWheelCollider, frontRightWheelTransform);
+        UpdateWheelPos(rearLeftWheelCollider, rearLeftWheelTransform);
+        UpdateWheelPos(rearRightWheelCollider, rearRightWheelTransform);
+    }
+
+    private void UpdateWheelPos(WheelCollider wheelCollider, Transform trans)
+    {
+        Vector3 pos;
+        Quaternion rot;
+        wheelCollider.GetWorldPose(out pos, out rot);
+        trans.rotation = rot;
+        trans.position = pos;
+    }
+
+
+    // WIP
+    private string Victoria(Transform carroTransform, int nivel)
+    {
+        Vector3 angulos = carroTransform.eulerAngles;
+
+        switch (nivel)
+        {
+            case 1:
+                if ((angulos.y >= 85 && angulos.y <= 90) || (angulos.y >= -85 && angulos.y <= -90))
+                {
+                    return "A+";
+                }
+                break;
+            case 2:
+                if ((angulos.y >= 85 && angulos.y <= 90) || (angulos.y >= -85 && angulos.y <= -90))
+                {
+                    return "A+";
+                }
+                break;
+            case 3:
+                if ((angulos.x == 0 && angulos.y == 0 && angulos.z == 0) ||
+                    (angulos.x == 0 && angulos.y == 180 && angulos.z == 0))
+                {
+                    return "A+";
+                }
+                break;
+            default:
+                return "Sin calificación";
+        }
+
+        return "B";
+    }
+
+
+}
